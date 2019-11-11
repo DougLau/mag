663ed5ad221f9ea3bc4b98c3a922137ff1690b7b
Doug Lau <doug.p.lau@gmail.com>
2019-11-11 12:10:02 -0600

    Improved example in README

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,19 @@
 //! ## Example
 //!
 //! ```rust
-//! use mag::length::{ft, m};
+//! use mag::{length::{ft, m, mi}, time::{h, s}};
 //!
 //! let a = 1.0 * ft; // Length<ft>
-//! let b = a.to::<m>(); // Length<m>
+//! let b = a.to::<m>(); // convert to Length<m>
+//! let c = 30.0 * s; // Period<s>
+//! let d = 60.0 / s; // Frequency<s>
+//! let e = 55.0 * mi / h; // Velocity<mi, h>
 //!
+//! assert_eq!(a.to_string(), "1 ft");
 //! assert_eq!(b.to_string(), "0.3048 m");
+//! assert_eq!(c.to_string(), "30 s");
+//! assert_eq!(d.to_string(), "60 ㎐");
+//! assert_eq!(e.to_string(), "55 mi/h");
 //! ```
 //!
 //! ## Room For Improvement
